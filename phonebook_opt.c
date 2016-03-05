1ed4c59b4b13a357be521fe6969d8aae62de7c6a
ruby0109 <ruby620412@gmail.com>
2016-03-05 22:54:40 +0800

    Update phonebook_opt.c

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -6,10 +6,24 @@
 entry *findName(char lastname[], entry *pHead)
 {
     /* TODO: implement */
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0){
+            printf(" %12s  is found!\n", lastName);
+            return pHead;
+        }
+        pHead = pHead->pNext;
+    }
+    printf(" %12s  is found!\n", lastName);
     return NULL;
 }
 
-entry *append(char lastName[], entry *e)
+entry *append(char lastName[], entry *ape)
 {
-    return NULL;
+    /* allocate memory for the new entry and put lastName */
+    ape->pNext = (lastNameEntry *) malloc(sizeof(lastNameEntry));
+    ape = ape->pNext;
+    strcpy(ape->lastName, lastName);
+    ape->pNext = NULL;
+
+    return ape;
 }
